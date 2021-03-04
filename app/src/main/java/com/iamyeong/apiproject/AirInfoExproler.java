d041d5e2bc66d8ddcceff9cab279711bf91b8359
IamYeong <wjdrhkddud2@gmail.com>
2021-03-04 23:47:17 +0900

    StringBuilder

--- app/src/main/java/com/iamyeong/apiproject/AirInfoExproler.java
+++ app/src/main/java/com/iamyeong/apiproject/AirInfoExproler.java
@@ -1,7 +1,33 @@
 package com.iamyeong.apiproject;
 
+import java.net.URLEncoder;
+
 public class AirInfoExproler {
     //API select method
+
+    private StringBuilder urlBuilder = new StringBuilder("http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty");
+
+    public AirInfoExproler() {
+
+    }
+
+    public StringBuilder select() {
+
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=서비스키"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
+        urlBuilder.append("&" + URLEncoder.encode("stationName","UTF-8") + "=" + URLEncoder.encode("종로구", "UTF-8")); /*측정소 이름*/
+        urlBuilder.append("&" + URLEncoder.encode("dataTerm","UTF-8") + "=" + URLEncoder.encode("DAILY", "UTF-8")); /*요청 데이터기간 (하루 : DAILY, 한달 : MONTH, 3달 :
+
+
+    }
+
+
+
+
+
+
+
 
 
 }
